d262c6dbb57030be73a5815f2b43e63e51e41d43
Guilherme Souza <guilhermedias.souza@outlook.com>
2026-08-10 13:18:19 -0300

    Exercicio com condicionais

---
+++ ex001/Condicionais/condi-3.js
@@ -0,0 +1,11 @@
+var idade = 1
+
+if (idade > 65) {
+    console.log('Você não precisa mais votar')
+} else if (idade >= 18) {
+    console.log('Voto Obrigatório')
+} else if (idade >= 16) {
+    console.log('Você ja pode votar')
+} else {
+    console.log('Você ainda não pode votar')
+}
