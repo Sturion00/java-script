414c181406dd86d157c6a254a65da15f11564d24
Guilherme Souza <257373090+Sturion00@users.noreply.github.com>
2026-08-18 10:35:17 -0300

    Primeiro debug (de muitos...)

---
+++ ex001/while-do-for.js
@@ -0,0 +1,30 @@
+/*
+n = 1
+j = 10
+
+while (n <= 5 ) {
+    console.log(`Opa, ${n}`)
+    n++
+}
+
+do {
+    console.log(`bão ${j}`)
+    j++
+} while (j <= 15 )
+*/
+
+/*
+c = 1
+
+while (c <= 10) {
+    console.log(c)
+    c++
+}
+*/
+
+c = 1
+
+for (c = 1; c <= 3; c++) {
+    console.log(`Tentativa ${c}`)
+}
+console.log('fim...')
