5e7736ac04fb66a69b5a21803965797adc72ad60
Guilherme Souza <257373090+Sturion00@users.noreply.github.com>
2026-08-25 16:12:53 -0300

    Exercicio 4 - Concluido
    com ajuda de ia

---
+++ Exerc.web/scripts/script04.js
@@ -0,0 +1,27 @@
+function calcular() {
+    const nasc = new Date(document.getElementById('txta').value)
+    const hoje = new Date()
+
+    let anos = hoje.getFullYear() - nasc.getFullYear()
+    let meses = hoje.getMonth() - nasc.getMonth()
+    let dias = hoje.getDate() - nasc.getDate()
+
+    if (dias < 0) {
+        meses--
+        dias += new Date(hoje.getFullYear(), hoje.getMonth(), 0).getDate()
+    }
+    if (meses < 0) {
+        anos--
+        meses += 12
+    }
+
+    alert(anos)
+
+    const saida = document.querySelector('div#saida>p.um')
+    saida.innerHTML = `Você tem ${anos} anos, ${meses} meses e ${dias} dias.`
+}
+
+
+
+
+
