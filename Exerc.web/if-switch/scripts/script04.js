e6da6e7fd556868aa70402e7cd0b4620a60f7a30
Guilherme Souza <257373090+Sturion00@users.noreply.github.com>
2026-09-02 17:05:46 -0300

    Exercicios if/switch 4- Concluido
    Escreva um código que recehba dois números e determine qual deles é o maior.

---
+++ Exerc.web/if-switch/scripts/script04.js
@@ -0,0 +1,8 @@
+function analisar() {
+    const num1 = Number(document.getElementById('txtn1').value)
+    const num2 = Number(document.getElementById('txtn2').value)
+    const saida = document.querySelector('div.saida>p')
+    
+    
+
+}
